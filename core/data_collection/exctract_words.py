3c285d377a476d8077ca556840cbdb4bf9c5c858
Saidi-Souhaieb <saidisouhaiebai@gmail.com>
2024-09-07 13:41:34 +0100

    added checking duplicated uuids when generating, made generate-images and exctract works with prints to keep track

--- core/data_collection/exctract_words.py
+++ core/data_collection/exctract_words.py
@@ -1,11 +1,16 @@
 import re
+import os
 import uuid
 import json
+import sys
+
 from utils.extract_verse_surah_numbers import extract_verse_surah_numbers
 from utils.filter_text import filter_text
 from utils.generate_uuid import generate_unique_id
 from utils.dump_json_data import dump_json_data
-from utils.pymongo_get_database import get_database
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from core.utils.pymongo_get_database import get_database
 
 dbname = get_database()
 quran_words_collection = dbname["quran_words"]
@@ -15,7 +20,7 @@ def generate_word_json():
     quran_verses_path = "../../static/text/quran_verses.txt"
     with open(quran_verses_path, "r") as quran_file:
         verses = quran_file.readlines()
-        verses = verses[6132:]
+        line_number = 0
         for verse in verses:
             try:
                 surah_number, verse_number = extract_verse_surah_numbers(verse)
@@ -30,6 +35,13 @@ def generate_word_json():
                     }
                     # dump_json_data(verse_data)
                     quran_words_collection.insert_one(verse_data)
+                    print(f"Finished generating and saving for line {line_number}")
+                    line_number += 1
             except Exception as e:
                 print(f"Exceptoin: {e}")
                 pass
+
+
+if __name__ == "__main__":
+    generate_word_json()
+    print("Finished exctracting words and saving to database!")
--- core/data_collection/generate_images.py
+++ core/data_collection/generate_images.py
@@ -1,7 +1,12 @@
 import csv
+import os
+import sys
+
 from PIL import ImageFont
 from utils.generate_image import generate_image
-from utils.pymongo_get_database import get_database
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from core.utils.pymongo_get_database import get_database
 
 dbname = get_database()
 quran_words_collection = dbname["quran_words"]
@@ -10,7 +15,10 @@ quran_words = quran_words_collection.find()
 
 def generate_images():
     font_names = ["eljaza", "hijazi", "naskh", "ruqah", "thuluth"]
+    number_words_generated = 0
     for font in font_names:
+        print(font)
+        # try:
         font_path = f"../../static/raw/fonts/{font}.ttf"
         csv_file_path = "../../static/csv/words_id.csv"
         font_size = 35
@@ -29,3 +37,15 @@ def generate_images():
                 writer.writerow([word_id])
 
             generate_image(text, word_id, font_object, font, text_y_offset=-4)
+            # print(f"Finished generating {number_words_generated} words!")
+            number_words_generated += 1
+        print(f"Finished generating words with font {font}")
+        quran_words.rewind()
+        # except Exception as e:
+        #     print(f"Exception: {e}")
+        #     pass
+
+
+if __name__ == "__main__":
+    generate_images()
+    print("Finished generating images with all fonts!")
